5c511cd19d6cd291a043dee805c25baeaeb2e5b5
Ana Ventura <anaventura1811@gmail.com>
2021-04-28 14:49:34 -0300

    Atualiza useState

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,34 @@
 import Header from './components/Header';
 import Tasks from './components/Tasks';
+import { useState } from 'react';
 
 function App() {
+  const [tasks] = useState([
+    { 
+      id: 1, 
+      text: 'Estudar',
+      day: 'Abr 29 às 10:30',
+      reminder: true
+    },
+    { 
+      id: 2, 
+      text: 'Revisar conteúdo',
+      day: 'Abr 30 às 10:30',
+      reminder: true
+    },
+    { 
+      id: 3, 
+      text: 'Comprar macarrão',
+      day: 'Mai 01 às 14:30',
+      reminder: true
+    },
+  ]
+)
 
   return (
     <div className="container">
       <Header />
-      <Tasks />
+      <Tasks tasks={tasks}/>
     </div>
   );
 }
--- my-app/src/components/Tasks.jsx
+++ my-app/src/components/Tasks.jsx
@@ -1,32 +1,15 @@
-import React, { useState }  from 'react';
+import React from 'react';
+import Task from './Task';
+
 
 // Primeiro cria-se uma lista, usando um array e método de array - map, filter, etc
 // Para cada item iterado da lista, é preciso adicionar uma key/chave, que no caso,
 // faz sentido que seja a id do objeto correspondente à cada item
-const Tasks = () => {
-
-  const [tasks, setTasks] = useState([
-      { 
-        id: 1, 
-        text: 'Estudar',
-        day: 'Abr 29 às 10:30'
-      },
-      { 
-        id: 2, 
-        text: 'Revisar conteúdo',
-        day: 'Abr 30 às 10:30'
-      },
-      { 
-        id: 3, 
-        text: 'Comprar macarrão',
-        day: 'Mai 01 às 14:30'
-      },
-    ]
-  )
+const Tasks = ({ tasks }) => {
 
   return (
     <>
-      {tasks.map((task) => (<h3 key={task.id}>{task.text}</h3>)
+      {tasks.map((task) => (<Task key={task.id} task={task}/>)
       )}
     </>
   )
