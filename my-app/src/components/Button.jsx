ca35e6747c19ef6337daf17bb79bfd46e9489528
Ana Ventura <anaventura1811@gmail.com>
2021-04-28 12:42:25 -0300

    Entendendo eventos no React

--- my-app/src/components/Button.jsx
+++ my-app/src/components/Button.jsx
@@ -2,7 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Button = ({ color, text }) => {
+
+  const onClick = () => {
+    console.log('Click, estou funfando')
+  }
   return <button className='btn' 
+    onClick={onClick}
     style={{backgroundColor: color}}>
       {text}
     </button>
